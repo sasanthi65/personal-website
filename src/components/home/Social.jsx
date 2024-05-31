24870423436b0abafb3e4dd967f6d6ee1149f6d8
sasanthi65 <sasanthi911@gmail.com>
2024-05-31 18:23:58 +0530

    changed social URL

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -7,7 +7,7 @@ function Social() {
             <i class="uil uil-github-alt"></i>
         </a>
 
-        <a href="https://www.linkedin.com/in/sasanthi-ranagala-955a191a6/" className="home_social-icon" target="_blank">
+        <a href="https://www.linkedin.com/in/sasanthi-lakmini/" className="home_social-icon" target="_blank">
         <i class="uil uil-linkedin-alt"></i>
         </a>
 
